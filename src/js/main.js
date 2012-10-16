e5aed0b20999bcc87db4dd74bc70a3b21b7ace15
Nathan Jones <thenathanjones@gmail.com>
2012-10-16 21:43:20 +0800

    Remove the autosize plugin.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,5 @@
 $(function() {
   var viewModel = new MainViewModel();
-  
-  viewModel.currentPage.subscribe(function() {
-    setTimeout(function() {$('.autosize').autosize();}, 250);
-  });
 
   ko.applyBindings(viewModel);
 
