6d5e9c7cd1c2428ddfbbdfdb106ece727c5e7489
Nathan Jones <thenathanjones@gmail.com>
2012-10-17 17:13:09 +0800

    Added callback to update the jQuery mobile styling on page changes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,9 @@
 $(function() {
   var viewModel = new MainViewModel();
 
+  viewModel.currentPage.subscribe(function() {
+    setTimeout(function() { $('#current-page').trigger('create'); }, 0);
+  });
+
   ko.applyBindings(viewModel);
-
-  // $.event.special.swipe.horizontalDistanceThreshold = 100;
-  // $.event.special.swipe.verticalDistanceThreshold = 100;
-
-  // $('body').bind('swipeleft', function() {
-  //   viewModel.swipeLeft();
-  // });
-
-  // $('body').bind('swiperight', function() {
-  //   viewModel.swipeRight();
-  // });
 });
